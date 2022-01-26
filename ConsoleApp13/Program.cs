4fc1f0bcf3e43ead72e4b19ab8a9ffcd74799a72
Filtey <erik89826502855@gmail.com>
2022-01-26 14:28:54 +0500

    Start

---
+++ ConsoleApp13/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0, b = 0;
+            while(true)
+            {
+                Console.WriteLine("\n1.Ввести А\n2.Ввести В\n3.Выполнить операцию -\n4.Выполнить операцию +\n5.Выполнить операцию *\n6.Выполнить операцию /");
+                int vvod = int.Parse(Console.ReadLine());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                Console.ReadKey();
+                Console.Clear();
+            }
+           
+        }
+    }
+}
